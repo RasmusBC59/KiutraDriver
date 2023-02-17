85807dde62dabf1fe4d9422be0d4fd1a936a75c4
Rasmus B. Christensen <rbcmail@gmail.com>
2023-02-17 16:54:43 +0100

    going home2

--- Kiutra.py
+++ Kiutra.py
@@ -7,12 +7,25 @@ import numpy as np
 
 from qcodes import validators as vals
 from qcodes.instrument import ChannelList, InstrumentChannel, Instrument
-from qcodes.parameters import Group, GroupParameter
+from qcodes.parameters import Parameter, Group, GroupParameter
 
 
 class Kiutra(Instrument):
     def __init__(name: str, address: str, **kwargs: Any):
-        super().__init__(name, address, **kwargs)
+        super().__init__(name, **kwargs)
+        host = adress
+        client = KiutraClient(host, 1006)
+        print(client.query('cryostat.status'))
+
+
+
+class Magnetigfield(Parameter):
+    __init__(name, host, **kwargs):    
+    sample_magnet = MagnetControl('sample_magnet', host)
+        
+    def get_raw(self):
+        npts = self.root_instrument.npts()
+        dt = self.root_instrument.dt()
 
 class Output(InstrumentChannel):
         def __init__(
